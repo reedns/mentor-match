bbe5fcb054249a4316843e50e7b9228344213abc
Philip Nguyen <supertaru@gmail.com>
2014-11-14 12:45:37 -0800

    Validates Course input for new user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :course, presence: true
   validates :email, presence: true
   validates :password, length: { minimum: 6 },
                        confirmation: true,
